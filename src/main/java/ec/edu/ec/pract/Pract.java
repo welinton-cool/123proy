c826fe35738914f371d9ebae797cb1d720614f37
welinton-cool <welinton.sacon@espoch.edu.ec>
2024-10-30 11:30:28 -0500

    Primer commit

---
+++ src/main/java/ec/edu/ec/pract/Pract.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.ec.pract;
+
+/**
+ *
+ * @author HP
+ */
+public class Pract {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
